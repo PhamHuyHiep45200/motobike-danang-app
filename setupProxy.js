1cec7e9d6f273367081ad346d348620f59607a33
hiep pham <hiep.pham@tomosia.com>
2023-06-21 20:22:42 +0900

    fix api

--- setupProxy.js
+++ setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api',
+    '/',
     createProxyMiddleware({
       target: 'http://13.211.94.23:5000/', // Đổi thành URL của NestJS
       changeOrigin: true,
